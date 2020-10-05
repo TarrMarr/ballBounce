85b7a12796cd83069ba892196e53b719ccdd425c
TarrMarr <66919385+TarrMarr@users.noreply.github.com>
2020-10-05 09:58:50 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+alert('test');
+
+let ball = document.getElementById('float-circle');
+
+function up(){
+  ball.style.bottom = '250px';
+}
+
+function down(){
+  ball.style.bottom = '50px';
+}
+
+document.onkeydown = up;
+document.onkeyup = down;
